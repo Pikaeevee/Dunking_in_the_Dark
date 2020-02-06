8dcf81784bb50c0f8c259e2c14effa9780c87447
Silvia Chen <suiqingc@andrew.cmu.edu>
2020-02-06 02:05:10 -0500

    created ball mvmt script + jump

---
+++ Dunking in the Dark/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float jumpSpeed = 8.0f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 currPos = transform.position;
+        currPos.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        transform.position = currPos; 
+
+        // jump button tbt, currently default 
+        if (Input.GetButton("Jump"))
+        {
+            // assuming ball is rigidbody 
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpSpeed); 
+        }
+
+    }
+}
