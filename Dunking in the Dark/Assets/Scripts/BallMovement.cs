4d7a4c658782d1ab85ab620cb553d66afd346c40
Silvia Chen <suiqingc@andrew.cmu.edu>
2020-02-06 02:30:48 -0500

    added to ballmvmt script to accomodate for icy sticky platforms

--- Dunking in the Dark/Assets/Scripts/BallMovement.cs
+++ Dunking in the Dark/Assets/Scripts/BallMovement.cs
@@ -5,27 +5,45 @@ using UnityEngine;
 public class BallMovement : MonoBehaviour
 {
     public float speed = 10.0f;
-    public float jumpSpeed = 8.0f; 
+    public float jumpSpeed = 8.0f;
+    public float stickySpeed = 2.0f; 
+
+    public bool onIce = false;
+    public bool onSticky = false; 
+
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 currPos = transform.position;
-        currPos.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float velocity = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 
-        transform.position = currPos; 
+        // check for different platforms 
+        if (onIce)
+        {
+            rb.AddForce(new Vector2(velocity, currPos.y));
+        }
+        else if (onSticky) {
+            speed = stickySpeed; 
+        }
+        else
+        {
+            transform.position = new Vector2(currPos.x + velocity, currPos.y); 
+        }
+
 
         // jump button tbt, currently default 
         if (Input.GetButton("Jump"))
         {
             // assuming ball is rigidbody 
-            GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpSpeed); 
+            rb.AddForce(Vector2.up * jumpSpeed); 
         }
 
     }
